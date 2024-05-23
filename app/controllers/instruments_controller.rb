8cde47e7b9bb0c4f6c2abca295fa0bbf896514a1
david gomes <davidgomesk8@gmail.com>
2024-05-23 15:52:05 +0200

    controller crash testing

---
+++ app/controllers/instruments_controller.rb
@@ -0,0 +1,47 @@
+class InstrumentsController < ApplicationController
+  before_action :set_instrument, only: [:show, :edit, :update, :destroy]
+  def index
+    @instruments = Instrument.all
+  end
+
+  def show
+  end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    if @instrument.save
+      redirect_to instrument_path(@instrument)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @instrument.update(instrument_params)
+    redirect_to instrument_path(@instrument)
+  end
+
+  def destroy
+    @instrument.destroy
+    redirect_to instruments_path
+  end
+
+
+  private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def instrument_params
+    params.require(:instrument).permit(:name, :category, :price)
+  end
+
+end
