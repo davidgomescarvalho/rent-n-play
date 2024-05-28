cef5142b2e1718fd3300768b7894b8e3a28f3db8
Hugo Ferrier <hu.ferrier@gmail.com>
2024-05-28 22:24:23 +0200

    Update datepicker_controller to use range mode and altInput

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,6 +4,16 @@ import flatpickr from "flatpickr";
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element, {
+      mode: "range",
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d",
+      minDate: "today",
+      defaultDate: this.element.value,
+      onChange: (selectedDates, dateStr, instance) => {
+        this.element.value = dateStr
+      }
+    })
   }
 }
